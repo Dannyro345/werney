5215886752ed2957df6820035a3c6bb206f79d77
Aluno <you@example.com>
2019-07-09 20:24:50 -0300

    Criando e implementando a pagina de listagem.

--- atendimento/models.py
+++ atendimento/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    cpf = models.CharField(max_length=11)
+    sexo = models.CharField(max_length=1)
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from . models import Cliente
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'cliente/list.html', {'clientes':clientes})
